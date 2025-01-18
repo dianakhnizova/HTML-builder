f5c108b1161b9cbeab90182d2b4b9220fb0e4ee3
dianakhnizova <khnizovad@gmail.com>
2025-01-18 15:34:19 +0600

    feat: add task - 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFile = path.join(__dirname, 'text.txt');
+
+const myReadText = fs.createReadStream(pathFile, 'utf-8');
+
+myReadText.on('data', function (chunk) {
+  console.log('New data received:\n' + chunk);
+});
+
+myReadText.on('error', (err) => {
+  console.error('Error reading the file:\n', err);
+});
+myReadText.on('end', () => {
+  console.log('File has been completely read.');
+});
