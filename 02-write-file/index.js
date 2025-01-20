b64f77de9f10b6ec7811a80c726acea2815e7780
dianakhnizova <khnizovad@gmail.com>
2025-01-20 10:43:07 +0600

    fix: fix task - 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
 const readLine = require('readline');
 
-const stdIn = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 const filePath = './02-write-file/newFile.txt';
 const newFile = fs.createWriteStream(filePath, {
   flags: 'a',
@@ -15,28 +10,28 @@ console.log('\nHello! Enter text to write to file:\n');
 console.log('Enter "exit" for exit\n');
 console.log('To interrupt, press "Ctrl + c"\n');
 
-stdIn.on('line', (input) => {
-  if (input === 'exit') {
-    stdIn.close();
-    process.exit(1);
-  }
-  newFile.write(input + '\n', (err) => {
-    if (err) {
-      console.error('\nError writing to file:', err);
-      stdIn.close();
-      process.exit();
-    }
-    console.log('\nThe text is written to the file!\nKeep typing...\n');
-  });
-});
-
-stdIn.on('close', () => {
+function message() {
   console.log('\nIt was completed! Goodbye!');
   newFile.end();
+  process.exit();
+}
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  if (input === 'exit') {
+    message();
+  } else {
+    newFile.write(input + '\n', (err) => {
+      if (err) {
+        console.error('\nError writing to file:', err);
+        stdIn.close();
+        process.exit();
+      }
+      console.log('\nThe text is written to the file!\nKeep typing...\n');
+    });
+  }
 });
 
 process.on('SIGINT', () => {
-  process.stdout.write('\nIt was completed! Goodbye!');
-  stdIn.close();
-  process.exit();
+  message();
 });
